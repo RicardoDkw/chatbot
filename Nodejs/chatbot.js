14fb5ebecf0ccc6ccfc5e8268bdb981da29de617
Ricardo Santos <ricardosilva9033@gmail.com>
2021-05-10 15:23:12 -0300

    chatbot-wittel

---
+++ Nodejs/chatbot.js
@@ -0,0 +1,43 @@
+const dialogflow = require("dialogflow");
+
+const dialogflowConfig = require("./config");
+const projectId = dialogflowConfig.projectId;
+const configuration = {
+  credentials: {
+    private_key: dialogflowConfig.privateKey,
+    client_email: dialogflowConfig.clientEmail
+  }
+};
+
+const sessionId = "987654";
+const languageCode = "en-US";
+const sessionClient = new dialogflow.SessionsClient(configuration);
+
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+async function talkToChatbot(message) {
+  const botRequest = {
+    session: sessionPath,
+    queryInput: {
+      text: {
+        text: message,
+        languageCode
+      }
+    }
+  };
+
+  const response = await sessionClient
+    .detectIntent(botRequest)
+    .then((responses) => {
+      console.log(responses[0].queryResult.fulfillmentText);
+      const requiredResponse = responses[0].queryResult;
+      return requiredResponse;
+    })
+    .catch((error) => {
+      console.log("ERROR: " + error);
+    });
+
+  return response;
+}
+
+module.exports = talkToChatbot;
